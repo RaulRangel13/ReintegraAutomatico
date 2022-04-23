1bb8a0a84b54e722a2340081dfb8b3b1f5429787
raul.rangel <raul.rangel@TI-NOTE-RRANGEL>
2022-04-23 20:39:37 -0300

    Add project files.

---
+++ ReintegraAutomatico/Program.cs
@@ -0,0 +1,85 @@
+﻿using ReintegrationSelf.Services;
+using System;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ReintegraAutomatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Login:
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Digite seu login");
+            string login = Console.ReadLine();
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Digite sua senha");
+            string senha = Console.ReadLine();
+            Console.WriteLine("--------------------------------------------");
+        //string login = "raul.rangel@snd.com.br";
+        //string senha = "1Cor15#33";
+
+        Restart:
+
+            ReintegraAutoService reintegraAutoService = new ReintegraAutoService();
+            reintegraAutoService.GoToScremLogin();
+
+            if(!reintegraAutoService.GoToScremLogin())
+            {
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine("Pagina não existe");
+                Console.WriteLine("Verifique se a pagina esta correta ou se o programa local esta rodando");
+                Console.WriteLine("Deseja tentar novamente?");
+                Console.WriteLine("s/n");
+                string go = Console.ReadLine();
+
+                if(go == "s")
+                {
+                   goto Restart;
+                }
+                else
+                {
+                   goto End;
+                }
+                
+            }
+            
+            reintegraAutoService.LogaAuto(login, senha);
+
+            if (reintegraAutoService.CheckScreamLogin())
+            {
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine("ATENÇÃO");
+                Console.WriteLine("Login ou Senha Invalidos");
+                Console.WriteLine("--------------------------------------------");
+                reintegraAutoService.CloseWindowns();
+                goto Login;
+            }
+            
+            reintegraAutoService.GoToReintegrationScrem();
+            reintegraAutoService.ReintegraAuto();
+
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("A parentemente todos os produtos foram reintegrados!!!");
+            Console.WriteLine("Deseja rodar novamente o ReintegrationSelf?");
+            Console.WriteLine("S/N");
+
+            string contunue = Console.ReadLine();
+
+            if(contunue == "s")
+            {
+                goto Restart;
+            }
+
+            reintegraAutoService.CloseWindowns();
+            reintegraAutoService.CloseWindowns();
+
+        End:
+            Environment.Exit(0);
+            
+        }
+    }
+}
---
+++ ReintegraAutomatico/Services/ReintegraAutoService.cs
@@ -0,0 +1,121 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReintegrationSelf.Services
+{
+    class ReintegraAutoService
+    {
+
+        ChromeDriver driver = new ChromeDriver();
+
+        public bool GoToScremLogin()
+        {
+            try
+            {
+                string url = "http://localhost:49915/";
+                driver.Navigate().GoToUrl(url);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            
+        }
+
+        public bool CheckScreamLogin()
+        {
+            try
+            {
+                driver.FindElement(By.Id("UserName"));
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        public void LogaAuto(string login, string password)
+        {
+            bool scremLogin = CheckScreamLogin();
+
+            if (scremLogin == true)
+            {
+                var fieldLogin = driver.FindElement(By.Id("UserName"));
+                var fieldSenha = driver.FindElement(By.Id("Password"));
+                var loginButton = driver.FindElement(By.CssSelector("#login-box > div > div.widget-main > form > fieldset > div.clearfix > input"));
+
+                fieldLogin.SendKeys(login);
+                fieldSenha.SendKeys(password);
+                loginButton.Click();
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine("ATENÇÃO");
+                Console.WriteLine("Você não esta na tela de login, reinicie o programa");
+                Console.WriteLine("--------------------------------------------");
+            }
+        }
+
+        public void GoToReintegrationScrem()
+        {
+            driver.SwitchTo().NewWindow(WindowType.Tab);
+            string url = "http://localhost:49915/Reintegracao";
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void ReintegraAuto()
+        {
+            //var abaReintegra = driver.FindElement(By.CssSelector("#sidebar > ul > li.open > ul > li > ul > li.open > ul > li:nth-child(1) > a"));
+            //abaReintegra.Click();
+            string results = "#_reintegracaoresult > div > div.row-fluid";
+            
+            int count = 0;
+
+            while (count < 30)
+            {
+                if (!ElementExist(results))
+                {
+                    count = 0;
+                    var reintegraButton = driver.FindElement(By.XPath("/html/body/div[3]/div[2]/div[2]/div[1]/div[2]/div/div/div/div/div/label/form/button"));
+                    reintegraButton.Click();
+                    Thread.Sleep(20000);
+                    driver.Navigate().Refresh();
+                }
+                else
+                {
+                    Thread.Sleep(6000);
+                    driver.Navigate().Refresh();
+                    count = count + 1;
+                }
+            }
+        }
+
+        public bool ElementExist(string cssElement)
+        {
+            try
+            {
+                driver.FindElement(By.CssSelector(cssElement));
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        public void CloseWindowns()
+        {
+            driver.Close();
+        }
+    }
+}
